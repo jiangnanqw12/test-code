fceef4d522670427acac1b62e7bee30847a946a5
jiangnanqw12 <382961818@qq.com>
2020-03-07 17:30:17 +0800

    20200307

--- pythonExcel/test20200307.py
+++ pythonExcel/test20200307.py
@@ -1,24 +1,66 @@
 
-f=open("data.txt","r")
-while 1:
-    line=f.readline()
-    if not line:
-        break
-    print(line[0])
-    print(type(line))
-    if line[4]=="0"&line[5]=="1"&line[8]=="0"&line[9]=="2"&line[12]=="0"&line[13]=="3":
-        date=line[0]+line[1]+line[2]+line[3]
-        lunch=line[6]+line[7]
-        dinner=line[10]+line[10]
-    for i in range(len(line)):
+def main():
+    fee=getFeeNumber()
+    for date in fee:
+        print(date)
+        print(fee[date])
+        print("date:",date,"lunch:",fee[date][0],"dinner:",fee[date][1])
+def getFree():
 
-        print("i:",i," ","value:",line[i])
-        
-    # line2=line.decode()
-    # print(line2)
-    # print(type(line2))
+def getFeeNumber():
+    f=open("data.txt","r")
+    fee=dict()
     
-# line=f.readline()
-# print(line[0])
+    countDinner=0
+    DinnerFlag=0
+    countLunch=0
+    LunchFlag=0
+    while 1:
+        line=f.readline()
+        if not line:
+            break
+        # print(line)
+        # print(type(line))
+        
+        if line[4]=="0" and line[5]=="1"  :
+            
+            MonthStr=line[0]+line[1]
+            DayStr=line[2]+line[3]
+            #month=eval(line[0]+line[1])
+            
+            #print(date)
+            
+            lunch=line[6]+line[7]
+            feeLunch=eval(lunch)
+            if feeLunch>0:
+                countLunch+=1
+                LunchFlag=1
+            
+            #print(lunch)
+            dinner=line[10]+line[11]
+            feeDinner=eval(dinner)
+            if feeDinner>0:
+                countDinner+=1
+                DinnerFlag=1
+            
+            #print(dinner)
+            
+            
+            fee[MonthStr+DayStr]=[feeLunch,feeDinner,LunchFlag,DinnerFlag]
+            
 
-f.close()
+        # for i in range(len(line)):
+
+        #     print("i:",i," ","value:",line[i])
+            
+        # line2=line.decode()
+        # print(line2)
+        # print(type(line2))
+        
+    # line=f.readline()
+    # print(line[0])
+
+    f.close()
+    return fee
+
+main()
