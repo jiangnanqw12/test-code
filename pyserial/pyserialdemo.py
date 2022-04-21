03d3ca082f4ced3c10419df7876f10b1bc6dd00b
Harry <382961818@qq.com>
2022-04-21 19:37:03 +0800

    SERIAL

--- pyserial/pyserialdemo.py
+++ pyserial/pyserialdemo.py
@@ -3,5 +3,14 @@ ser = serial.Serial()
 ser.baudrate = 115200
 ser.port = "COM2"
 ser.open()
-a = 12345
-ser.write(a)
+a = "1"
+packet = bytearray()
+packet.append(173)
+packet.append(0x7b)
+ser.write(packet)
+ser.write(b"\x12")
+while True:
+    s = ser.read()
+    print(s)
+
+# ser.send(b"1")
