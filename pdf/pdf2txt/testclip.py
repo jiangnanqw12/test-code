2a2026b620a4400ac60e5ef1ca3789871f962d1f
shade <382961818@qq.com>
2022-09-17 14:56:15 +0800

    modified:   pdf/pdf2txt/testclip.py

--- pdf/pdf2txt/testclip.py
+++ pdf/pdf2txt/testclip.py
@@ -5,12 +5,16 @@ import os
 # path = os.getcwd()+"/"
 # f1=open(path+"test.txt","w")
 # f1.write(text)
-def copy_text_from_clip(path,end_file):
+
+
+def copy_text_from_clip(path, end_file):
     text = pyperclip.paste()
-    print("clip:",text)
+    # print("clip:",text)
     #path = os.getcwd()+"/"
-    f1=open(path+"re."+end_file,"w",encoding="utf-8")
+    f1 = open(path+"re."+end_file, "w", encoding="utf-8")
     f1.write(text)
+
+
 if __name__ == '__main__':
     path = os.getcwd()+"/"
-    copy_text_from_clip(path,"pdftext")
+    copy_text_from_clip(path, "pdftext")
