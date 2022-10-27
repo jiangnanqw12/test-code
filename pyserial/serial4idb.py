a8e8ce72cf539b6f700cfbcea3e8e59c8d7494b9
shade <382961818@qq.com>
2022-10-27 19:28:27 +0800

    modified:   pyserial/generateTestData.py 	modified:   pyserial/serial4idb.py

--- pyserial/serial4idb.py
+++ pyserial/serial4idb.py
@@ -72,6 +72,7 @@ def get_send_list(data_send,listsend):
 def send_data(data_send,listsend):
     listsend=[]
     get_send_list(data_send,listsend)
+
     ser.write(listsend)
 def get_crc():
     s=str(synccode)+str(cmd)+str(des)+str(data1)+str(data2)
@@ -92,21 +93,18 @@ def hex2str(hexdata):
     elif len(strData)<10:
         #print(10*"0")
         return (10-len(strData))*"0"+strData[2:]
-#def send_synccode():
-
-if __name__ == '__main__':
+def test_case1():
     synccode=0xAD7BC565
     cmd=0x00180100
     des=0x00b00000
     data1=0x037a037c
     data2=0x037e0380
     check=0XA6D5D986
-
-    print(serial_ports())
+    global ser
     ser=serial.Serial("COM4",57600,timeout=1)
     if ser.is_open!=True:
         print("can't open ")
-    str_send=""
+    str_send=''
     listsend=[]
     send_data(synccode,listsend)
     str_send+=hex2str(synccode)
@@ -126,9 +124,135 @@ if __name__ == '__main__':
     send_data(check,listsend)
     rs=ser.read(100)
     print(rs)
-
+    for i in rs:
+        print(hex(i))
     print(str_send)
+def send_synccode(send_data_hex):
+    str_send=''
+    listsend=[]
+    send_data(send_data_hex,listsend)
+    str_send+=hex2str(send_data_hex)
+    return str_send
+def generate_cmd(config):
+    data_frame_lenght=4+4+4+config[1]*2+4
+    #print(hex(data_frame_lenght)+"0100")
+    #cmd=eval(hex(data_frame_lenght)+"0100")
+    #print(data_frame_lenght)
+    cmd=(data_frame_lenght<<16)+0x0100
+    #print(hex(cmd))
+    return cmd,data_frame_lenght
+def send_data_hex_in4(send_data_hex):
+    str_send=''
+    listsend=[]
+    send_data(send_data_hex,listsend)
+    str_send+=hex2str(send_data_hex)
+    return str_send
+def generate_des(i,column):
+    base1=i*0x00080000
+    base2=base1+column*0x400
+    return base2
+def generate_send_list(linelist,row,column):
+    send_list=[]
+    temp=""
+    for i in range(row):
 
+        if (i+1)%2==0:
+            temp2=eval(linelist[column*config[0]+i])
+            send_list.append((temp2<<16)+temp1)
+        else:
+            temp1=eval(linelist[column*config[0]+i])
+    #print(send_list)
+
+    return send_list
+def test1():
+    data1=123
+    data2=12
+    data3=(123<<16)+data2
+    print(data3)
+def hex_data_2_bytes_list(hex_data,data_frame_length):
+    list_send=[]
+    for i in range(data_frame_length):
+        temp=(hex_data>>(data_frame_length*8-(i+1)*8))&0xff
+        list_send.append(temp)
+    return list_send
+
+
+def store_data(hex_data_send,data):
+    hex_data_send=(hex_data_send<<32)+data
+    #print(hex(hex_data_send))
+    return hex_data_send
+def test_case2():
+    global synccode
+    synccode=0xAD7BC565
+    cmd=0x00180100
+    des=0x00b00000
+    data1=0x037a037c
+    data2=0x037e0380
+    check=0XA6D5D986
+    calibritionNA=[256,256,132,2,1,"calibritionNA"]
+    testcase1=[4,4,3,9,1,"testcase1"]
+    print(serial_ports())
+    global ser
+    ser=serial.Serial("COM4",57600,timeout=1)
+    if ser.is_open!=True:
+        print("can't open ")
+    global str_send
+    global listsend
+    str_send=""
+    listsend=[]
+    global config
+    config=testcase1
+
+    for i in range(config[2]):
+        f= open(config[5]+"/"+str(i+1)+".txt",encoding="utf-8")
+        linelist = f.readlines()
+
+
+        for column in range(config[0]):
+            hex_data_send=synccode
+
+            #print(str_send)
+            cmd_code,data_frame_length=generate_cmd(config)
+            #print(data_frame_length)
+            #print(hex(cmd_code),config)
+            hex_data_send=store_data(hex_data_send,cmd_code)
+
+            #print(str_send)
+            des_code=generate_des(i,column)
+            hex_data_send=store_data(hex_data_send,des_code)
+            #hex_data_send=(hex_data_send<<32)+des_code
+            #print(hex(hex_data_send))
+            #print(des_code)
+            # str_send_temp=send_data_hex_in4(des_code)
+            # str_send+=str_send_temp
+            #print(str_send)
+            #for row in range(config[1]):
+            send_list=generate_send_list(linelist,config[0],column)
+            #print(send_list)
+            for d in send_list:
+                hex_data_send=store_data(hex_data_send,d)
+
+            #print(hex(hex_data_send))
+            check_code=crc32mpeg2(binascii.a2b_hex(hex(hex_data_send)[2:]))
+            #print(hex(check_code))
+            hex_data_send=store_data(hex_data_send,check_code)
+
+            #print(hex(hex_data_send))
+            #send_data_hex_in4(s3)
+            temp=hex_data_send
+            list_send=hex_data_2_bytes_list(hex_data_send,data_frame_length)
+            # print(list_send)
+            # for d1 in list_send:
+            #     print(hex(d1))
+            ser.write(list_send)
+            rs=ser.read(100)
+            print(rs)
+            #print(hex(rs[-5]))
+            # for i in rs:
+            #     print(hex(i))
+            #send_data_list()
+            # generate_check()
+            # send_check()
     #print(0xAD7BC5650018010000B00000037A037C037E0380)
     # s4=binascii.crc32mpeg2(binascii.a2b_hex('AD'))
     # print(hex(s4))
@@ -137,9 +261,98 @@ if __name__ == '__main__':
 
     # get_crc()
 
-    print(hex(int((len(str_send)+8)/2)))
-    s3=crc32mpeg2(binascii.a2b_hex('AD7BC5650018010000B00000037A037C037E0380'))
-    print(hex(s3))
-    s3=crc32mpeg2(binascii.a2b_hex(str_send))
-    print(hex(s3))
+    # print(hex(int((len(str_send)+8)/2)))
+    # s3=crc32mpeg2(binascii.a2b_hex('AD7BC5650018010000B00000037A037C037E0380'))
+    # print(hex(s3))
+    # s3=crc32mpeg2(binascii.a2b_hex(str_send))
+    # print(hex(s3))
+def chip_erase():
+    synccode=0xAD7BC565
+    hex_data_send=synccode
+    config=[0,0,0,0,1,"test2"]
+    cmd_code,data_frame_length=generate_cmd(config)
+    hex_data_send=store_data(hex_data_send,cmd_code)
+    hex_data_send=store_data(hex_data_send,0x0)
+    check_code=0x7d731943
+    hex_data_send=store_data(hex_data_send,check_code)
+    list_send=hex_data_2_bytes_list(hex_data_send,data_frame_length)
+    ser.write(list_send)
+    rs=ser.read(100)
+    print(rs)
+def writ_data_4IDB():
+    global synccode
+    synccode=0xAD7BC565
+    cmd=0x00180100
+    des=0x00b00000
+    data1=0x037a037c
+    data2=0x037e0380
+    check=0XA6D5D986
+    calibritionNA=[256,256,132,2,1,"calibritionNA"]
+    fastAlign=[256,256,24,4,1,"fastAlign"]
+    fasZernike=[256,256,46,5,1,"fastZernike"]
+    pupil=[256,256,8,6,1,"pupil"]
+    testcase1=[4,4,3,9,1,"testcase1"]
+    config_list=[calibritionNA,fastAlign,fasZernike,pupil]
+    print(serial_ports())
+    global ser
+    ser=serial.Serial("COM4",57600,timeout=1)
+    if ser.is_open!=True:
+        print("can't open ")
+    global str_send
+    global listsend
+    str_send=""
+    listsend=[]
+    global config
+    #config=testcase1
+    for config in config_list:
+        for i in range(config[2]):
+            print("start to wirite %d pic"&(i+1))
+            f= open(config[5]+"/"+str(i+1)+".txt",encoding="utf-8")
+            linelist = f.readlines()
+
+
+            for column in range(config[0]):
+                hex_data_send=synccode
+
+                #print(str_send)
+                cmd_code,data_frame_length=generate_cmd(config)
+                #print(data_frame_length)
+                #print(hex(cmd_code),config)
+                hex_data_send=store_data(hex_data_send,cmd_code)
+
+                #print(str_send)
+                des_code=generate_des(i,column)
+                hex_data_send=store_data(hex_data_send,des_code)
+                #hex_data_send=(hex_data_send<<32)+des_code
+                #print(hex(hex_data_send))
+                #print(des_code)
+                # str_send_temp=send_data_hex_in4(des_code)
+                # str_send+=str_send_temp
+                #print(str_send)
+                #for row in range(config[1]):
+                send_list=generate_send_list(linelist,config[0],column)
+                #print(send_list)
+                for d in send_list:
+                    hex_data_send=store_data(hex_data_send,d)
+
+                #print(hex(hex_data_send))
+                check_code=crc32mpeg2(binascii.a2b_hex(hex(hex_data_send)[2:]))
+                #print(hex(check_code))
+                hex_data_send=store_data(hex_data_send,check_code)
+
+                #print(hex(hex_data_send))
+                #send_data_hex_in4(s3)
+                temp=hex_data_send
+                list_send=hex_data_2_bytes_list(hex_data_send,data_frame_length)
+                # print(list_send)
+                # for d1 in list_send:
+                #     print(hex(d1))
+                ser.write(list_send)
+                rs=ser.read(100)
+                print(rs)
+
+if __name__ == '__main__':
+    #test1()
+    writ_data_4IDB()
+    #test_case1()
 
