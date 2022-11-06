b66d5e9fadc18c7015de1b2167c635352480b0e1
shade <382961818@qq.com>
2022-11-06 22:03:38 +0800

    modified:   you-get/re.downlist 	modified:   you-get/youget_download_4text.py

--- you-get/youget_download_4text.py
+++ you-get/youget_download_4text.py
@@ -1,10 +1,10 @@
 import os
 
 
-def down_one(format_str, url, down_path=r'c:\bili'):
+def down_one(format_str, url, down_path=r'c:\bili', proxy=r"--no-proxy"):
 
-    cmd = """you-get {} {} -o {} --no-proxy""".format(
-        format_str, url, down_path)
+    cmd = """you-get {} {} -o {} {} --debug""".format(
+        format_str, url, down_path, proxy)
     print(cmd)
     os.system(cmd)
 
@@ -16,4 +16,7 @@ down_path = os.getcwd()
 f1 = open(down_path+"/re.downlist", 'r')
 linelist = f1.readlines()
 for line in linelist:
-    down_one("-l", line[:-2], down_path=r'c:\bili')
+    down_one("", "\""+line[:-1]+"\"", down_path=r'c:\bili',
+             proxy="-s 127.0.0.1:1024")
+    # down_one("", "\""+line[:-1]+"\"", down_path=r'c:\bili',
+    #          proxy="--socks-proxy 127.0.0.1:10809")
