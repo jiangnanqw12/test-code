d4c34b607d50e376d25d941e6397aaa5d7ad8f6d
Harry <382961818@qq.com>
2023-06-25 13:10:11 +0800

    modified:   001_markdown/md_process.py     wiki_note_process

--- 001_markdown/md_process.py
+++ 001_markdown/md_process.py
@@ -12,137 +12,6 @@ import aspose.words as aw
 import urllib.parse
 
 
-def remove_filesname_end(path, old_extension, new_extension, file_remove_length=0, dir_remove_length=0):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(old_extension):
-                os.replace(os.path.join(root, file),
-                           os.path.join(root, file[:-(len(old_extension)+file_remove_length)]+new_extension))
-        for dir in dirs:
-            if dir_remove_length > 0:
-                os.replace(os.path.join(root, dir), os.path.join(
-                    root, dir[:-(dir_remove_length)]))
-
-
-def rename_files_start(num_remove, extensions=None):
-    # 获取当前目录下所有文件的文件名
-    files = os.listdir()
-
-    for filename in files:
-        # 如果文件名以"."开头，说明是隐藏文件，跳过
-        if filename.startswith("."):
-            continue
-
-        # 获取文件名的后缀名
-        suffix = os.path.splitext(filename)[-1]
-        # 如果设置了扩展名过滤器，且当前文件扩展名不在过滤器中，跳过
-        if extensions and suffix not in extensions:
-            continue
-        # 获取文件名的新名称（去除前四位）
-        new_filename = filename[num_remove:]
-
-        # 生成文件的旧路径和新路径
-        old_path = os.path.join(os.getcwd(), filename)
-        new_path = os.path.join(os.getcwd(), new_filename)
-
-        # 重命名文件
-        os.replace(old_path, new_path)
-
-        # 输出修改后的文件名
-        print("文件名从 {} 变为 {}".format(filename, new_filename))
-
-
-def rename_files(path, replace_list):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_old = file
-            for replace_str in replace_list:
-                if file.find(replace_str[0]) != -1:
-                    file = file.replace(replace_str[0], replace_str[1])
-            os.replace(os.path.join(root, file_old),
-                       os.path.join(root, file))
-        for dir in dirs:
-            dir_old = dir
-            for replace_str in replace_list:
-                if dir.find(replace_str[0]) != -1:
-                    dir = dir.replace(replace_str[0], replace_str[1])
-            os.replace(os.path.join(root, dir_old),
-                       os.path.join(root, dir))
-
-# remove line with search_str
-
-
-def remove_line(path, search_list):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith('.md'):
-                with open(os.path.join(root, file), 'r', encoding='UTF-8') as f:
-                    lines = f.readlines()
-                with open(os.path.join(root, file), 'w', encoding='UTF-8') as f:
-                    for l in lines:
-                        flag = 0
-                        for search_str in search_list:
-                            if l.find(search_str) != -1:
-                                flag = flag+1
-                        if flag == 0:
-                            f.write(l)
-
-
-def mulu_process(replace_list):
-    with open("目录.md", 'r', encoding='UTF-8') as f:
-        lines = f.readlines()
-    with open("目录.md", 'w', encoding='UTF-8') as f:
-        for l in lines:
-            for replace_str in replace_list:
-                if l.find(replace_str[0]) != -1:
-                    l = l.replace(replace_str[0], replace_str[1])
-            f.write(l)
-
-
-# def num2str_title(num):
-#     if num < 10:
-#         return '00'+str(num)
-#     elif num < 100:
-#         return '0'+str(num)
-#     else:
-#         return str(num)
-
-
-def search_file(path):
-    with (open("目录.md", 'r', encoding='UTF-8')) as f:
-        lines = f.readlines()
-        for root, dirs, files in os.walk(path):
-            counter = 0
-            for line in lines:
-                if line[:-1]+'.md' in files:
-                    counter = counter+1
-                    os.replace(os.path.join(
-                        root, line[:-1]+'.md'), os.path.join(root, num2str_title(counter)+line[:-1]+'.md'))
-                    print(line[:-1]+'.md')
-
-
-def global_replace_list():
-    global replace_list
-    replace_list = [["「", "_"], ["」", "_"], [
-        "，", ","], ["：", "_"], [" ", "_"], ["“", "_"], ["”", "_"], ["？", "?"], ["·", "_"]]
-
-
-def remove_line_test1():
-    path = os.getcwd()
-    search_list = ["- created: 2023", "- source: https://www.zhihu.com"]
-    remove_line(path, search_list)
-
-
-
-def tianjiaxiahuaxian():
-    for root, dirs, files in os.walk("."):
-        for i in range(1, 38):
-            for file in files:
-                if file[:3] == num2str_title(i):
-                    os.replace(os.path.join(root, file),
-                               os.path.join(root, file[:3]+"_"+file[3:]))
-
-
 def back_up_dir_tree(path):
 
     time_str = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
@@ -174,92 +43,6 @@ def back_up_dir(src_dir):
         if os.path.isfile(src_path):
             shutil.copy2(src_path, dst_path)
 
-# remove multiple lines with space
-
-
-def remove_line_multi_space(path):
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith('.md'):
-                with open(os.path.join(root, file), 'r', encoding='UTF-8') as f:
-                    lines = f.readlines()
-                with open(os.path.join(root, file), 'w', encoding='UTF-8') as f:
-                    for i in range(len(lines)):
-                        space_list = ["\n", "    \n"]
-                        if i != len(lines)-1:
-                            if lines[i] == ' ' and lines[i+1] == ' ':
-                                continue
-                            if lines[i] in space_list and lines[i+1] in space_list:
-                                continue
-                        f.write(lines[i])
-
-# copy all files in the current path into a single file
-
-
-def copy_all_files(path):
-    with open("all_files.md", 'w', encoding='UTF-8') as f:
-        for root, dirs, files in os.walk(path):
-            for i in range(1, 38):
-                for file in files:
-                    if file[:3] == num2str_title(i) and file.endswith('.md'):
-                        with open(os.path.join(root, file), 'r', encoding='UTF-8') as f1:
-                            lines = f1.readlines()
-                        for line in lines:
-                            f.write(line)
-
-
-def process_feiman_head2():
-    for root, dirs, files in os.walk("."):
-        for i in range(1, 38):
-            for file in files:
-                if file[:3] == num2str_title(i) and file.endswith('.md'):
-                    with open(os.path.join(root, file), 'r', encoding='UTF-8') as f:
-                        lines = f.readlines()
-                    with open(os.path.join(root, file), 'w', encoding='UTF-8') as f:
-                        for i in range(len(lines)):
-                            if lines[i][0:3] == "## " and lines[i+1] != "\n":
-                                f.write(lines[i][:-1])
-                            else:
-                                f.write(lines[i])
-
-
-def search_in_mulu():
-    counter = 0
-    with open("mulu.md", 'r', encoding='UTF-8') as f:
-        lines = f.readlines()
-    for root, dirs, files in os.walk("."):
-        for line in lines:
-            if line[:-1]+'.md' in files:
-                counter = counter+1
-                # print(line[:-1]+'.md')
-
-            else:
-                print(line[:-1]+'.md')
-        # print(files)
-
-
-# def rename_files_base_on_index_markdown(path=None):
-#     counter = 0
-#     if path is None:
-#         path = os.getcwd()
-#     with open(os.path.join(path,"000_index.md"), 'r', encoding='UTF-8') as f:
-#         lines = f.readlines()
-#     files = os.listdir(path)
-#     for i in range(len(lines)):
-#         line =lines[i]
-#         line = line.replace("%", "_")
-#     #for line in lines:
-#         if line[-1]=="\n":
-#             note_name=line[:-1]+'.md'
-#         else:
-#             note_name=line+'.md'
-#         if note_name in files:
-#             counter = i+1
-#             # print(line[:-1]+'.md')
-#             os.replace(os.path.join(path, note_name),
-#                         os.path.join(path, num2str_title(counter)+"_"+note_name))
-#         else:
-#             raise Exception(note_name+" not in the files list")
 
 
 def num2str_title(num):
@@ -284,33 +67,6 @@ def rename_files_base_on_index_markdown(path=None):
             raise FileNotFoundError(f"{note_name} not in the files list")
 
 
-def create_md_files_from_markdown_file(markdown_file):
-    """
-    读取markdown文件，将每一行内容生成一个md文件，并根据行号、内容、扩展名为文件命名。
-    如果该文件已经存在，则跳过该行，继续生成下一行的文件。
-
-    :param markdown_file: 要读取的markdown文件名
-    """
-    with open(markdown_file, encoding='utf-8') as f:
-        lines = f.readlines()
-        for i, line in enumerate(lines):
-            # 构造文件名
-            filename = num2str_title(i+1)+f"_{line.strip()}.md"
-            # 检查文件是否已存在，如果是则跳过
-            if os.path.exists(filename):
-                continue
-            # 创建新的md文件并写入内容
-            with open(filename, 'w', encoding='utf-8') as new_file:
-                # new_file.write(line)
-                pass
-
-
-
-
-def delete_non_example_md_files():
-    for file_path in glob.glob('*.md'):
-        if file_path != 'example.md' and file_path.endswith('.md'):
-            os.remove(file_path)
 
 
 def add_timestamp_to_filenames():
@@ -398,15 +154,7 @@ def mdx2md(timestamp: int = 1676880280):
             for i in range(len(replace_list_regex)):
                 pattern = replace_list_regex[i][0]
                 replacement = replace_list_regex[i][1]
-                # pattern2=r'''<Question[\n \t]{0,}question="(.+)"([\n \t]{0,}choice\d="(.+)"){1,}[\n \t]{0,}answer=\{(\d)\}[\n \t]{0,}>'''
-                # match=re.search(pattern2, content)
-                # if match:
-                #     print(len(match.groups()))
-                #     # print(match.group(0))
-                #     # print(match.group(1))
-                #     # print(match.group(2))
-                #     print(match.group(3))
-                #     #print(match.group(4))
+
 
                 # Perform the regex replacement
                 content = re.sub(pattern, replacement, content)
@@ -1689,6 +1437,34 @@ def remove_back_matter_and_copy_code(directory_path=None):
     reg_string_list.extend([reg_string_copy_code])
     perform_regex_replacement_on_files(reg_string_list, directory_path, files_md)
 
+def wiki_note_process(num=0):
+    operations = {
+        1: remove_wiki_edit_link,
+
+
+    }
+
+    if num in operations:
+        operations[num]()
+    elif num == 0:
+        print("Available operations:")
+        for num, func in operations.items():
+            print(f"{num}: {func.__name__}")
+    else:
+        raise ValueError("Invalid operation number.")
+def remove_wiki_edit_link(directory_path=None):
+
+    if directory_path is None:
+        directory_path = os.getcwd()
+
+    files_md = [f for f in os.listdir(directory_path) if f.endswith('.md')]
+
+    reg_string_list=[]
+
+    reg_wiki_edit_link=[r'\\\[\[edit\]\(https://en\.wikipedia\.org/w/index\.php\?title=.+ "Edit section: (.+)"\)\\\]',r""]
+    reg_string_list.extend([reg_wiki_edit_link])
+
+    perform_regex_replacement_on_files(reg_string_list, directory_path, files_md)
 def main():
     # create a parser object
     parser = argparse.ArgumentParser()
@@ -1715,6 +1491,8 @@ def main():
                         action='store_true', help='call remove_filesname_end')
     parser.add_argument('-md', '--md_note_process',
                         action='store_true', help='call md_note_process')
+    parser.add_argument('-wiki', '--wiki_note_process',
+                        action='store_true', help='call wiki_note_process')
     parser.add_argument('-tt', '--timestamps_3blue1brown_2_timeline',
                         action='store_true', help='call timestamps_3blue1brown_2_timeline')
     parser.add_argument('-cti', '--copy_timestamps_and_index_2_root',
@@ -1753,6 +1531,8 @@ def main():
     # call the appropriate function based on the arguments
     if args.md_note_process:
         md_note_process(args.input_int)
+    elif args.wiki_note_process:
+        wiki_note_process(args.input_int)
     elif args.copy_timestamps_and_index_2_root:
         copy_timestamps_and_index_2_root()
     elif args.convert_subtitle_chatgpt_summary_to_markdown_vid_timeline:
