be00c492bd752b8902bbe69a5910b8b2e6b2e45e
Harry <382961818@qq.com>
2023-07-31 14:27:28 +0800

    deleted:    001_markdown/flag.py 	new file:   001_markdown/flags.py

--- 001_markdown/flag.py
+++
@@ -1,3 +0,0 @@
-
-class flag_note():
-    flag_bat = 0
---
+++ 001_markdown/flags.py
@@ -0,0 +1,29 @@
+class GlobalFlags:
+    def __init__(self):
+        self.flags = {
+            'debug': False,
+            'verbose': False,
+            'feature_x_enabled': True,
+        }
+
+    def set_flag(self, name, value):
+        if name in self.flags:
+            self.flags[name] = value
+        else:
+            raise KeyError(f"Flag '{name}' not found.")
+
+    def get_flag(self, name):
+        return self.flags.get(name, None)
+
+    def toggle_flag(self, name):
+        if name in self.flags:
+            self.flags[name] = not self.flags[name]
+        else:
+            raise KeyError(f"Flag '{name}' not found.")
+if __name__ == '__main__':
+    # Usage
+    flags = GlobalFlags()
+    flags.set_flag('debug', True)
+    print(flags.get_flag('debug')) # Output: True
+    flags.toggle_flag('debug')
+    print(flags.get_flag('debug')) # Output: False
