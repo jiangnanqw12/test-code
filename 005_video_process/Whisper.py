afa7232d824b1f30a1011b1110c01cf312b11772
shade <382961818@qq.com>
2023-09-19 14:02:50 +0800

    deleted:    002_tools/testcode/201911EMC/desktop.ini 	modified:   005_video_process/Whisper.py 	modified:   005_video_process/ffmpeg.py 	renamed:    007_settings/yt-dlp/www.youtube.com_cookies.txt -> 005_video_process/yt-dlp/www.youtube.com_cookies.txt 	renamed:    007_settings/yt-dlp/yt-dlp.conf -> 005_video_process/yt-dlp/yt-dlp.conf 	renamed:    007_settings/yt-dlp/yt-dlp_bak.py -> 005_video_process/yt-dlp/yt-dlp_bak.py 	renamed:    007_settings/yt-dlp/yt-dlp_bili.conf -> 005_video_process/yt-dlp/yt-dlp_bili.conf 	renamed:    007_settings/yt-dlp/yt-dlp_subprocess.py -> 005_video_process/yt-dlp/yt-dlp_subprocess.py 	renamed:    007_settings/yt-dlp/yt.downlist -> 005_video_process/yt-dlp/yt.downlist

--- 005_video_process/Whisper.py
+++ 005_video_process/Whisper.py
@@ -1,6 +1,25 @@
 import whisper
-#whisper audio.flac --model medium --language Japanese
-model = whisper.load_model("medium")
-result = model.transcribe("audio.mp3")
-with open("test", "w", encoding="utf-8") as f1:
-    f1.write(result)
+# whisper audio.flac --model medium --language Japanese
+import subprocess
+
+
+def convert_video(input_file, output_file, crf=23, preset='slower'):
+    command = [
+        "ffmpeg",
+        "-i", input_file,
+        "-vcodec", "libx264",
+        "-crf", str(crf),
+        "-preset", preset,
+        output_file
+    ]
+
+    result = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if result.returncode == 0:
+        print("Conversion was successful")
+        return True
+    else:
+        print("Conversion failed")
+        print("Error:", result.stderr.decode('utf-8'))
+        return False
--- 005_video_process/ffmpeg.py
+++ 005_video_process/ffmpeg.py
@@ -1,20 +1,55 @@
 import ffmpeg
 import os
-#ffmpeg -i *.mp4 -vcodec libx264 -crf 23 -preset slower output/*.mp4
+# ffmpeg -i *.mp4 -vcodec libx264 -crf 23 -preset slower output/*.mp4
+
+
+import ffmpeg
+from pathlib import Path
+import subprocess
+
+
+def convert_video(input_file, output_file, crf=23, preset='slower'):
+    command = [
+        "ffmpeg",
+        "-i", input_file,
+        "-vcodec", "libx264",
+        "-crf", str(crf),
+        "-preset", preset,
+        output_file
+    ]
+
+    result = subprocess.run(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if result.returncode == 0:
+        print("Conversion was successful")
+        return True
+    else:
+        print("Conversion failed")
+        print("Error:", result.stderr.decode('utf-8'))
+        return False
+
 
 def mp4_process(path=None):
-    if path is None:
-        path=os.getcwd()
+    # If path is None, get the current directory
+    path = Path(path) if path else Path.cwd()
 
-    files_mp4 = [f for f in os.listdir(path) if os.path.isfile(
-                os.path.join(path, f)) and (f.endswith(".mp4"))]
-    for file in files_mp4:
-        input_file = os.path.join(path,file)
-        output_file = os.path.join(path,"output",file)
-        ffmpeg.input(input_file).output(output_file, vcodec='libx264', crf=23, preset='slower').run()
+    output_dir = path / "output"
+    # Create output directory if it doesn't exist
+    output_dir.mkdir(exist_ok=True)
+
+    # Use glob to get all .mp4 files
+    files_mp4 = list(path.glob("*.mp4"))
+
+    for input_file in files_mp4:
+        output_file = output_dir / input_file.name
+        convert_video(input_file, output_file)
+        # ffmpeg.input(str(input_file)).output(str(output_file), vcodec='libx264', crf=23, preset='slower').run()
+
 
 def main():
     mp4_process()
+
 
 if __name__ == "__main__":
     main()
