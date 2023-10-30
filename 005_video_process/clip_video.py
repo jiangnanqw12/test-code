366c236d882fad9187b690a9423105f5c8cb98c3
shade <382961818@qq.com>
2023-10-30 14:57:57 +0800

    modified:   005_video_process/clip_video.py 	new file:   005_video_process/yt-dlp/downlist/CS50.downlist 	renamed:    005_video_process/yt-dlp/downlist/tan3.downlist -> 005_video_process/yt-dlp/downlist/bak/tan3.downlist

--- 005_video_process/clip_video.py
+++ 005_video_process/clip_video.py
@@ -1,30 +1,38 @@
-import os
-from moviepy.editor import*
-image_list = ["flv", "mp4"]
-path = os.getcwd()
-begin_time = [0, 0, 1]
-end_time = [0, 0, 2]
-output_file_name = "前期对线狗熊"
-# filenamelist[-2][19:28]
+import subprocess
 
 
-def split_video_fix_range(path4video):
+def split_video(video_path, start_time, duration, output_file):
+    """
+    Split video using ffmpeg with re-encoding.
 
-    for (dirpath, dirnames, filenames) in os.walk(path4video):
-        if filenames != []:
-            for filename in filenames:
-                filenamelist = filename.split(".")
-                if(filenamelist[-1] in image_list):
-                    print("in")
-                    video = CompositeVideoClip([VideoFileClip(dirpath + "/"+filename
-                                                              ).subclip(t_start=(begin_time[0], begin_time[1], begin_time[2]), t_end=(end_time[0], end_time[1], end_time[2]))])
-                    for i in range(len(filenamelist[-2])):
-                        if filenamelist[-2][i] == "年":
-                            if filenamelist[-2][i-4:i] == "2022":
-                                video.write_videofile(
-                                    output_file_name+filenamelist[-2][i-5:i+5]+".mp4")
-                            else:
-                                print(filenamelist[-2][i-4:i])
+    :param video_path: Path to the input video file.
+    :param start_time: Start time in 'hh:mm:ss' format.
+    :param duration: Duration of the segment in 'hh:mm:ss' format.
+    :param output_file: Name of the output file.
+    """
+
+    cmd = [
+        'ffmpeg',
+        '-i', video_path,
+        '-ss', start_time,
+        '-t', duration,
+        '-c:v', 'libx264',  # Force video re-encoding using libx264
+        '-c:a', 'aac',      # Force audio re-encoding using aac
+        '-strict', 'experimental',
+        output_file
+    ]
+
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"Segment saved as {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error splitting video: {e}")
 
 
-split_video_fix_range(path)
+if __name__ == '__main__':
+    video_path = "002_How Ultrasonic Energy Impacts Tissue.mp4"
+    start_time = "00:02:12"
+    duration = "00:00:9"
+    output_file = "Vessel sealing process.mp4"
+
+    split_video(video_path, start_time, duration, output_file)
