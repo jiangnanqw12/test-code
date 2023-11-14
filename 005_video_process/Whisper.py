bb9f41bc7335607ef35b89ae80ab6a201ee745fb
shade <382961818@qq.com>
2023-11-14 21:44:10 +0800

    modified:   005_video_process/Whisper.py

--- 005_video_process/Whisper.py
+++ 005_video_process/Whisper.py
@@ -1,25 +1,39 @@
-import whisper
+
 # whisper audio.flac --model medium --language Japanese
 import subprocess
 
+import subprocess
+import os
 
-def convert_video(input_file, output_file, crf=23, preset='slower'):
-    command = [
-        "ffmpeg",
-        "-i", input_file,
-        "-vcodec", "libx264",
-        "-crf", str(crf),
-        "-preset", preset,
-        output_file
-    ]
 
-    result = subprocess.run(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def get_subtitles(video_directory=None):
+    # Define the directory containing video files
+    video_directory = video_directory or os.getcwd()
 
-    if result.returncode == 0:
-        print("Conversion was successful")
-        return True
-    else:
-        print("Conversion failed")
-        print("Error:", result.stderr.decode('utf-8'))
-        return False
+    # Get all video files in the directory
+    video_files = [f for f in os.listdir(
+        video_directory) if f.endswith('.mp4')]
+
+    # Iterate over each video file
+    for video_file in video_files:
+        full_path = os.path.join(video_directory, video_file)
+        # Construct the Whisper command with the medium model option
+        command = ["whisper", full_path, "--model",
+                   "medium", "--language", "zh"]
+        # Execute the Whisper command using subprocess
+        result = subprocess.run(command)
+        if result.returncode == 0:
+            print("Conversion was successful")
+            return True
+        else:
+            print("Conversion failed")
+            print("Error:", result.stderr.decode('utf-8'))
+            return False
+
+
+def main():
+    get_subtitles()
+
+
+if __name__ == "__main__":
+    main()
