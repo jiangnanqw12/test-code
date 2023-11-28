d40214e0ab54123f5d1cbab505f8f071243923b4
shade <382961818@qq.com>
2023-11-28 16:22:34 +0800

    modified:   002_tools/curl_download.py

--- 002_tools/curl_download.py
+++ 002_tools/curl_download.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 import os
 import urllib.parse
 
+# List of allowed extensions
+ALLOWED_EXTENSIONS = ['.c', '.h', '.csv', '.py', '.db', '.zip', '.html', '.png',
+                      '.jpg', '.pdf', '.vtt', '.srt', '-360p.mp4']
+
 
 def download_file(url, local_path, base_path=None):
     download_file_curl(url, local_path, base_path)
@@ -15,8 +19,9 @@ def download_file_curl(url, local_path, base_path=None):
         decoded_url = urllib.parse.unquote(url)  # First, decode the URL
         encoded_url = urllib.parse.quote(
             decoded_url, safe=':/')  # Then, encode it properly
+        # if extension_filter(url):
         subprocess.run(['curl', '-o', local_path, '-L', '-v',
-                       '-k', '-A', 'Mozilla/5.0', encoded_url], check=True)
+                        '-k', '-A', 'Mozilla/5.0', encoded_url], check=True)
     except subprocess.CalledProcessError as e:
         if base_path == None:
             base_path = os.getcwd()
@@ -55,7 +60,13 @@ def download_file_requests(url, local_path, base_path=None):
 
 
 def is_valid_link(href):
+    # Check if the link is valid
+
     return href and not href.startswith('?') and not href.startswith('/') and not href.startswith('../')
+
+
+def extension_filter(url):
+    return url and any(url.endswith(ext) for ext in ALLOWED_EXTENSIONS)
 
 
 def download_from_url(base_url, target_directory, visited_urls=set()):
@@ -79,11 +90,11 @@ def download_from_url(base_url, target_directory, visited_urls=set()):
             else:
                 local_file_path = os.path.join(target_directory, href)
                 os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
-                download_file(download_url, local_file_path)
+                download_from_url(download_url, local_file_path)
 
 
 def main():
-    base_url = r'https://downloads.freemdict.com/%E5%B0%9A%E6%9C%AA%E6%95%B4%E7%90%86/%E5%85%B1%E4%BA%AB2020.5.11/content/1_english/'  # Replace with your URL
+    base_url = r'https://cdn.cs50.net/2022/fall/'  # Replace with your URL
     # Replace with your desired local directory
     target_directory = os.getcwd()
 
