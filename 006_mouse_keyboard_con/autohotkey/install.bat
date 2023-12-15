ed46f36c383adc43174f16ea77986248ced64d0a
shade <382961818@qq.com>
2023-12-15 16:44:57 +0800

    deleted:    006_mouse_keyboard_con/autohotkey/install.bat 	new file:   006_mouse_keyboard_con/autohotkey/obsidian_potplayer/config.ini 	new file:   006_mouse_keyboard_con/autohotkey/obsidian_potplayer/lib/MyTool.ahk 	new file:   006_mouse_keyboard_con/autohotkey/obsidian_potplayer/lib/ReduceTime.ahk 	new file:   006_mouse_keyboard_con/autohotkey/obsidian_potplayer/note2potplayer.ahk 	new file:   006_mouse_keyboard_con/autohotkey/obsidian_potplayer/potplayer2note.ahk 	deleted:    006_mouse_keyboard_con/autohotkey/potplayer.ahk 	deleted:    006_mouse_keyboard_con/autohotkey/potplayer_ori.ahk 	deleted:    006_mouse_keyboard_con/autohotkey/uninstall.bat 	deleted:    "006_mouse_keyboard_con/autohotkey/\345\246\202\344\275\225\344\275\277\347\224\250.pdf"

--- 006_mouse_keyboard_con/autohotkey/install.bat
+++
@@ -1,6 +0,0 @@
-reg add HKEY_CURRENT_USER\Software\Classes\ahk-potplayer /ve /d "" /f
-reg add HKEY_CURRENT_USER\Software\Classes\ahk-potplayer /v "URL Protocol" /d "" /f
-reg add HKEY_CURRENT_USER\Software\Classes\ahk-potplayer\Shell /ve /f
-reg add HKEY_CURRENT_USER\Software\Classes\ahk-potplayer\Shell\Open /ve /f
-
-reg add HKEY_CURRENT_USER\Software\Classes\ahk-potplayer\Shell\Open\command /ve /d "\"%~dp0potplayer.exe\" \"%%1\"" /f
--- 006_mouse_keyboard_con/autohotkey/uninstall.bat
+++
@@ -1,1 +0,0 @@
-reg delete HKEY_CURRENT_USER\Software\Classes\ahk-potplayer /f
