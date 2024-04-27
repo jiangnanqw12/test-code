0725f6707bcfc4df71c37dfba1f765bd4c37ff8f
shade <382961818@qq.com>
2024-04-27 10:10:28 +0800

    modified:   007_settings/chocolate/packages.config 	new file:   fotmat_container/test.cpp

---
+++ fotmat_container/test.cpp
@@ -0,0 +1,6 @@
+// shared_ptr that points to an int with value 42
+shared_ptr<int> p3 = make_shared<int>(42);
+// p4 points to a string with value 9999999999
+shared_ptr<string> p4 = make_shared<string>(10, '9');
+// p5 points to an int that is value initialized (§ 3.3.1 (p. 98)) to 0
+shared_ptr<int> p5 = make_shared<int>();
