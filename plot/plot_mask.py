9c2c8a28eff90310b1eea9244fbb1fc63fa8a768
shade <jiangnanqw12@gmail.com>
2024-06-01 16:54:52 +0800

    class LED(CircleComponentMatplotlib):     pass
    class GlassSubstrate(RectangleComponentMatplotlib):
        pass
    
    class ProtectionFrame(RectangleComponentMatplotlib):
        pass
    
    class Pin(RectangleComponentMatplotlib):
        pass
    	modified:   plot/plot_mask.py

--- plot/plot_mask.py
+++ plot/plot_mask.py
@@ -25,32 +25,29 @@ LED_1_DIAMETER = 2
 # glass_substrate_1
 GLASS_SUBSTRATE_1_LENGTH = 152
 GLASS_SUBSTRATE_1_WIDTH = 6.35
-GLASS_SUBSTRATE_1_LED_1_DELTA_X = \
-    - (GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER/2)
-GLASS_SUBSTRATE_1_LED_1_DELTA_Y = (7 + GLASS_SUBSTRATE_1_WIDTH / 2)
+GLASS_SUBSTRATE_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
+GLASS_SUBSTRATE_1_LED_1_DELTA_Y = 7 + GLASS_SUBSTRATE_1_WIDTH / 2
 GLASS_SUBSTRATE_1_X = GLASS_SUBSTRATE_1_LED_1_DELTA_X + LED_1_X
 GLASS_SUBSTRATE_1_Y = GLASS_SUBSTRATE_1_LED_1_DELTA_Y + LED_1_Y
 
 # protection_frame_1
 PROTECTION_FRAME_1_LENGTH = 122
 PROTECTION_FRAME_1_WIDTH = 6
-PROTECTION_FRAME_1_LED_1_DELTA_X = \
-    - (GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER/2)
-PROTECTION_FRAME_1_LED_1_DELTA_Y = (7 - PROTECTION_FRAME_1_WIDTH / 2)
+PROTECTION_FRAME_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
+PROTECTION_FRAME_1_LED_1_DELTA_Y = 7 - PROTECTION_FRAME_1_WIDTH / 2
 PROTECTION_FRAME_1_X = PROTECTION_FRAME_1_LED_1_DELTA_X + LED_1_X
 PROTECTION_FRAME_1_Y = PROTECTION_FRAME_1_LED_1_DELTA_Y + LED_1_Y
 
 # pin_1
 PIN_1_LENGTH = 6
 PIN_1_WIDTH = 2
-PIN_1_LED_1_DELTA_X = -(PIN_1_WIDTH/2+1.5+2+30 +
-                        GLASS_SUBSTRATE_1_LENGTH+30+1.5+LED_1_DIAMETER/2)
-PIN_1_LED_1_DELTA_Y = 7+7
-PIN_1_X = PIN_1_LED_1_DELTA_X+LED_1_X
-PIN_1_Y = PIN_1_LED_1_DELTA_Y+LED_1_Y
+PIN_1_LED_1_DELTA_X = -(PIN_1_WIDTH / 2 + 1.5 + 2 + 30 + GLASS_SUBSTRATE_1_LENGTH + 30 + 1.5 + LED_1_DIAMETER / 2)
+PIN_1_LED_1_DELTA_Y = 7 + 7
+PIN_1_X = PIN_1_LED_1_DELTA_X + LED_1_X
+PIN_1_Y = PIN_1_LED_1_DELTA_Y + LED_1_Y
 
 # slot_height
-SLOT_HEIGHT=19.05
+SLOT_HEIGHT = 19.05
 
 class CircleComponentMatplotlib:
     def __init__(self, x, y, diameter, edgecolor='white', facecolor='white'):
@@ -61,10 +58,8 @@ class CircleComponentMatplotlib:
         self.facecolor = facecolor
 
     def draw(self, ax):
-        circle = patches.Circle((self.x, self.y), self.radius,
-                                edgecolor=self.edgecolor, facecolor=self.facecolor)
+        circle = patches.Circle((self.x, self.y), self.radius, edgecolor=self.edgecolor, facecolor=self.facecolor)
         ax.add_patch(circle)
-
 
 class RectangleComponentMatplotlib:
     def __init__(self, center_x, center_y, length, width, edgecolor='white', facecolor='black'):
@@ -76,9 +71,20 @@ class RectangleComponentMatplotlib:
         self.facecolor = facecolor
 
     def draw(self, ax):
-        rectangle = patches.Rectangle(
-            (self.x, self.y), self.length, self.width, edgecolor=self.edgecolor, facecolor=self.facecolor)
+        rectangle = patches.Rectangle((self.x, self.y), self.length, self.width, edgecolor=self.edgecolor, facecolor=self.facecolor)
         ax.add_patch(rectangle)
+
+class LED(CircleComponentMatplotlib):
+    pass
+
+class GlassSubstrate(RectangleComponentMatplotlib):
+    pass
+
+class ProtectionFrame(RectangleComponentMatplotlib):
+    pass
+
+class Pin(RectangleComponentMatplotlib):
+    pass
 
 def main():
     # Create a plot
@@ -92,17 +98,11 @@ def main():
     ax.set_aspect('equal', adjustable='box')
 
     # Create the components
-    led_1 = CircleComponentMatplotlib(LED_1_X, LED_1_Y, LED_1_DIAMETER)
+    led_1 = LED(LED_1_X, LED_1_Y, LED_1_DIAMETER)
 
-    glass_substrate_1 = RectangleComponentMatplotlib(GLASS_SUBSTRATE_1_X, GLASS_SUBSTRATE_1_Y,
-                                                    GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_WIDTH)
-    protection_frame_1 = RectangleComponentMatplotlib(PROTECTION_FRAME_1_X, PROTECTION_FRAME_1_Y,
-                                                    PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_WIDTH)
-    pin_1 = RectangleComponentMatplotlib(PIN_1_X,    PIN_1_Y,
-                                        PIN_1_WIDTH, PIN_1_LENGTH, facecolor='white'
-                                        )
-
-
+    glass_substrate_1 = GlassSubstrate(GLASS_SUBSTRATE_1_X, GLASS_SUBSTRATE_1_Y, GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_WIDTH)
+    protection_frame_1 = ProtectionFrame(PROTECTION_FRAME_1_X, PROTECTION_FRAME_1_Y, PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_WIDTH)
+    pin_1 = Pin(PIN_1_X, PIN_1_Y, PIN_1_WIDTH, PIN_1_LENGTH, facecolor='white')
 
     # Draw the components
     led_1.draw(ax)
