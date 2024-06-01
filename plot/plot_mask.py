ff748eb76b22883fa73209bfec11ef19968b1ce1
shade <jiangnanqw12@gmail.com>
2024-06-01 17:22:26 +0800

    Key Enhancements: Parameterize Slot Height: Added slot_height as a parameter to the main function for flexibility. Configuration Section: Grouped all constants together at the top of the script. Docstrings: Added docstrings to classes and the main function to improve documentation. Readability: Improved code readability and maintainability by structuring the code clearly. 	modified:   plot/plot_mask.py

--- plot/plot_mask.py
+++ plot/plot_mask.py
@@ -15,14 +15,10 @@ import matplotlib.patches as patches
 # matplotlib x
 # the circle's center
 
-# 定义常量
-# mm
-
-# led_1
+# Configuration constants (all lengths are in mm)
 LED_1_X, LED_1_Y = 178.2, 40.524
 LED_1_DIAMETER = 2
 
-# glass_substrate_1
 GLASS_SUBSTRATE_1_LENGTH = 152
 GLASS_SUBSTRATE_1_WIDTH = 6.35
 GLASS_SUBSTRATE_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
@@ -30,7 +26,6 @@ GLASS_SUBSTRATE_1_LED_1_DELTA_Y = 7 + GLASS_SUBSTRATE_1_WIDTH / 2
 GLASS_SUBSTRATE_1_X = GLASS_SUBSTRATE_1_LED_1_DELTA_X + LED_1_X
 GLASS_SUBSTRATE_1_Y = GLASS_SUBSTRATE_1_LED_1_DELTA_Y + LED_1_Y
 
-# protection_frame_1
 PROTECTION_FRAME_1_LENGTH = 122
 PROTECTION_FRAME_1_WIDTH = 6
 PROTECTION_FRAME_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
@@ -38,7 +33,6 @@ PROTECTION_FRAME_1_LED_1_DELTA_Y = 7 - PROTECTION_FRAME_1_WIDTH / 2
 PROTECTION_FRAME_1_X = PROTECTION_FRAME_1_LED_1_DELTA_X + LED_1_X
 PROTECTION_FRAME_1_Y = PROTECTION_FRAME_1_LED_1_DELTA_Y + LED_1_Y
 
-# pin_1
 PIN_1_LENGTH = 6
 PIN_1_WIDTH = 2
 PIN_1_LED_1_DELTA_X = -(PIN_1_WIDTH / 2 + 1.5 + 2 + 30 + GLASS_SUBSTRATE_1_LENGTH + 30 + 1.5 + LED_1_DIAMETER / 2)
@@ -46,7 +40,6 @@ PIN_1_LED_1_DELTA_Y = 7 + 7
 PIN_1_X = PIN_1_LED_1_DELTA_X + LED_1_X
 PIN_1_Y = PIN_1_LED_1_DELTA_Y + LED_1_Y
 
-# slot_height
 SLOT_HEIGHT = 19.05
 
 class CircleComponentMatplotlib:
@@ -58,6 +51,7 @@ class CircleComponentMatplotlib:
         self.facecolor = facecolor
 
     def draw(self, ax):
+        """Draw the circle on the given axes."""
         circle = patches.Circle((self.x, self.y), self.radius, edgecolor=self.edgecolor, facecolor=self.facecolor)
         ax.add_patch(circle)
 
@@ -71,6 +65,7 @@ class RectangleComponentMatplotlib:
         self.facecolor = facecolor
 
     def draw(self, ax):
+        """Draw the rectangle on the given axes."""
         rectangle = patches.Rectangle((self.x, self.y), self.length, self.width, edgecolor=self.edgecolor, facecolor=self.facecolor)
         ax.add_patch(rectangle)
 
@@ -86,8 +81,8 @@ class ProtectionFrame(RectangleComponentMatplotlib):
 class Pin(RectangleComponentMatplotlib):
     pass
 
-def main():
-    # Create a plot
+def main(slot_height=SLOT_HEIGHT):
+    """Main function to create and display the plot."""
     fig, ax = plt.subplots()
 
     # Set plot limits and background color
@@ -96,25 +91,23 @@ def main():
     fig.patch.set_facecolor('black')
     ax.set_facecolor('black')
     ax.set_aspect('equal', adjustable='box')
+
+    # Create and draw components
     for i in range(6):
+        led = LED(LED_1_X, LED_1_Y - i * slot_height, LED_1_DIAMETER)
+        glass_substrate = GlassSubstrate(GLASS_SUBSTRATE_1_X, GLASS_SUBSTRATE_1_Y - i * slot_height,
+                                         GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_WIDTH)
+        protection_frame = ProtectionFrame(PROTECTION_FRAME_1_X, PROTECTION_FRAME_1_Y - i * slot_height,
+                                           PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_WIDTH)
+        pin = Pin(PIN_1_X, PIN_1_Y - i * slot_height, PIN_1_WIDTH, PIN_1_LENGTH, facecolor='white')
 
-        # Create the components
-        led_1 = LED(LED_1_X, LED_1_Y-(i*SLOT_HEIGHT), LED_1_DIAMETER)
+        led.draw(ax)
+        glass_substrate.draw(ax)
+        protection_frame.draw(ax)
+        pin.draw(ax)
 
-        glass_substrate_1 = GlassSubstrate(GLASS_SUBSTRATE_1_X, GLASS_SUBSTRATE_1_Y-(i*SLOT_HEIGHT), GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_WIDTH)
-        protection_frame_1 = ProtectionFrame(PROTECTION_FRAME_1_X, PROTECTION_FRAME_1_Y-(i*SLOT_HEIGHT), PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_WIDTH)
-        pin_1 = Pin(PIN_1_X, PIN_1_Y-(i*SLOT_HEIGHT), PIN_1_WIDTH, PIN_1_LENGTH, facecolor='white')
-
-        # Draw the components
-        led_1.draw(ax)
-        glass_substrate_1.draw(ax)
-        protection_frame_1.draw(ax)
-        pin_1.draw(ax)
-
-    # Hide the axes
+    # Hide the axes and show the plot
     ax.axis('off')
-
-    # Show the plot
     plt.show()
 
 if __name__ == "__main__":
