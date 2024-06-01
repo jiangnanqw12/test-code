6b286853e5fe2d899282946136fe40875ad8cead
shade <jiangnanqw12@gmail.com>
2024-06-01 10:20:35 +0800

    modified:   plot/plot_mask.py

--- plot/plot_mask.py
+++ plot/plot_mask.py
@@ -5,7 +5,7 @@ import matplotlib.patches as patches
 fig, ax = plt.subplots()
 
 # 设置绘图区域范围
-ax.set_xlim(0, 400)
+ax.set_xlim(-50, 200)
 ax.set_ylim(0, 100)
 
 # 设置背景颜色为黑色
@@ -22,18 +22,22 @@ led_1_circle = patches.Circle((led_1_x, led_1_y), led_1_diameter / 2, edgecolor=
 ax.add_patch(led_1_circle)
 
 # glass_substrate_1 rectangle
-delta_x_1, delta_y_1 = (30 + 1 + 152 / 2), (7 + 6.35 / 2)
+delta_x_1, delta_y_1 = -(30 + 1 + 152 / 2), (7 + 6.35 / 2)
 center_x_1, center_y_1 = (delta_x_1 + led_1_x), (delta_y_1 + led_1_y)
+glass_substrate_1_rect_length=152
+glass_substrate_1_rect_width=6.35
 glass_substrate_1_rect = patches.Rectangle(
-    (center_x_1 - 152 / 2, center_y_1 - 6.35 / 2), 152, 6.35, edgecolor='white', facecolor='black'
+    (center_x_1 - glass_substrate_1_rect_length / 2, center_y_1 - glass_substrate_1_rect_width / 2), glass_substrate_1_rect_length, glass_substrate_1_rect_width, edgecolor='white', facecolor='black'
 )
 ax.add_patch(glass_substrate_1_rect)
 
 # protection_frame_1 rectangle
-delta_x2_1, delta_y2_1 = (30 + 1 + 152 / 2), (7 - 6 / 2)
+delta_x2_1, delta_y2_1 = -(30 + 1 + 152 / 2), (7 - 6 / 2)
 center_x_2, center_y_2 = (delta_x2_1 + led_1_x), (delta_y2_1 + led_1_y)
+protection_frame_1_rect_length=122
+protection_frame_1_rect_width=6
 protection_frame_1_rect = patches.Rectangle(
-    (center_x_2 - 122 / 2, center_y_2 - 6 / 2), 122, 6, edgecolor='white', facecolor='black'
+    (center_x_2 - protection_frame_1_rect_length / 2, center_y_2 - protection_frame_1_rect_width / 2), protection_frame_1_rect_length, protection_frame_1_rect_width, edgecolor='white', facecolor='black'
 )
 ax.add_patch(protection_frame_1_rect)
 
