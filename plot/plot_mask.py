5ccb2c0834f635ee2aa5998ec49892ba4b520ed7
shade <jiangnanqw12@gmail.com>
2024-06-03 15:01:43 +0800

    ADD GLASS_SUBSTRATE_1_Z LED_1_Z PROTECTION_FRAME_1_Z 	modified:   plot/plot_mask.py

--- plot/plot_mask.py
+++ plot/plot_mask.py
@@ -1,46 +1,65 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-# X2  X3
-#   X
-# X1  X4
+"""
+X2  X3
+  X
+X1  X4
 
-# Rectangle
-# Inkscape x2
-# matplotlib x1
+Rectangle
+Inkscape x2
+matplotlib x1
 
-# Circle
-# Inkscape x2
-# the top-left point of the bounding box
-# matplotlib x
-# the circle's center
+Circle
+Inkscape x2
+the top-left point of the bounding box
+matplotlib x
+the circle's center
+"""
 
 # Configuration constants front_view (all lengths are in mm)
+
+TUBAN=6
+OTHER_SLOT_HEIGHT = 19.05
+
 LED_1_X, LED_1_Y = 150, 50
+LED_1_Z=0
 LED_1_DIAMETER = 2
 
 GLASS_SUBSTRATE_1_LENGTH = 152
-GLASS_SUBSTRATE_1_WIDTH = 6.35
+GLASS_SUBSTRATE_1_WIDTH = 152
+GLASS_SUBSTRATE_1_HEIGHT = 6.35
 GLASS_SUBSTRATE_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
-GLASS_SUBSTRATE_1_LED_1_DELTA_Y = 7 + GLASS_SUBSTRATE_1_WIDTH / 2
+GLASS_SUBSTRATE_1_LED_1_DELTA_Y = 7 + GLASS_SUBSTRATE_1_HEIGHT / 2
+GLASS_SUBSTRATE_1_LED_1_DELTA_Z = TUBAN + GLASS_SUBSTRATE_1_WIDTH / 2
 GLASS_SUBSTRATE_1_X = GLASS_SUBSTRATE_1_LED_1_DELTA_X + LED_1_X
 GLASS_SUBSTRATE_1_Y = GLASS_SUBSTRATE_1_LED_1_DELTA_Y + LED_1_Y
+GLASS_SUBSTRATE_1_Z = GLASS_SUBSTRATE_1_LED_1_DELTA_Z + LED_1_Z
 
 PROTECTION_FRAME_1_LENGTH = 122
-PROTECTION_FRAME_1_WIDTH = 6
-PROTECTION_FRAME_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
-PROTECTION_FRAME_1_LED_1_DELTA_Y = 7 - PROTECTION_FRAME_1_WIDTH / 2
-PROTECTION_FRAME_1_X = PROTECTION_FRAME_1_LED_1_DELTA_X + LED_1_X
+PROTECTION_FRAME_1_WIDTH = 149
+PROTECTION_FRAME_1_HEIGHT = 6
+#PROTECTION_FRAME_1_LED_1_DELTA_X = -(GLASS_SUBSTRATE_1_LENGTH / 2 + 30 + 1.5 + LED_1_DIAMETER / 2)
+PROTECTION_FRAME_1_LED_1_DELTA_Y = 7 - PROTECTION_FRAME_1_HEIGHT / 2
+
+#PROTECTION_FRAME_1_X = PROTECTION_FRAME_1_LED_1_DELTA_X + LED_1_X
+PROTECTION_FRAME_1_X = GLASS_SUBSTRATE_1_X
 PROTECTION_FRAME_1_Y = PROTECTION_FRAME_1_LED_1_DELTA_Y + LED_1_Y
+PROTECTION_FRAME_1_Z = GLASS_SUBSTRATE_1_Z
 
 PIN_1_LENGTH = 6
 PIN_1_WIDTH = 2
+PIN_1_HEIGHT = 6
 PIN_1_LED_1_DELTA_X = -(PIN_1_WIDTH / 2 + 1.5 + 2 + 30 + GLASS_SUBSTRATE_1_LENGTH + 30 + 1.5 + LED_1_DIAMETER / 2)
 PIN_1_LED_1_DELTA_Y = 7 + 7
 PIN_1_X = PIN_1_LED_1_DELTA_X + LED_1_X
 PIN_1_Y = PIN_1_LED_1_DELTA_Y + LED_1_Y
+PIN_1_Z = LED_1_Z
 
-OTHER_SLOT_HEIGHT = 19.05
+
+
+
+
 
 class CircleComponentMatplotlib:
     def __init__(self, x, y, diameter, edgecolor='white', facecolor='white'):
@@ -82,21 +101,26 @@ class Pin(RectangleComponentMatplotlib):
     pass
 
 def plot_front_view_mask(ax, slot_height, num_components):
-    """Plot the front view mask with the given slot height and number of components."""
+    """Plot the front view (XY) mask with the given slot height and number of components."""
     for i in range(num_components):
         led = LED(LED_1_X, LED_1_Y - i * slot_height, LED_1_DIAMETER)
         glass_substrate = GlassSubstrate(GLASS_SUBSTRATE_1_X, GLASS_SUBSTRATE_1_Y - i * slot_height,
-                                         GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_WIDTH)
+                                         GLASS_SUBSTRATE_1_LENGTH, GLASS_SUBSTRATE_1_HEIGHT)
         protection_frame = ProtectionFrame(PROTECTION_FRAME_1_X, PROTECTION_FRAME_1_Y - i * slot_height,
-                                           PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_WIDTH)
+                                           PROTECTION_FRAME_1_LENGTH, PROTECTION_FRAME_1_HEIGHT)
         pin = Pin(PIN_1_X, PIN_1_Y - i * slot_height, PIN_1_WIDTH, PIN_1_LENGTH, facecolor='white')
 
         led.draw(ax)
         glass_substrate.draw(ax)
         protection_frame.draw(ax)
         pin.draw(ax)
-    led_mapping=LED(LED_1_X, LED_1_Y + slot_height, LED_1_DIAMETER)
+    # todo ues same slot_height?
+    led_mapping=LED(LED_1_X, LED_1_Y + slot_height, LED_1_DIAMETER,facecolor='red')
     led_mapping.draw(ax)
+
+def plot_top_view_mask():
+    """XZ"""
+    pass
 def main():
     xlim=(-72.8, 152)
     ylim=(-47.5, 74)
